0c02fdefc52310d3c34e46e6f1b77cc2999ee5b3
Caique Daniel <caiquedaniel@outlook.com.br>
2023-03-01 00:02:34 -0300

    ERI-414 Criar layout da listagem de animais

--- src/app/Certvet.tsx
+++ src/app/Certvet.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import FormProntuario from "./views/form-prontuario/FormProntuario";
-
 import "./certvet.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
 import "@fortawesome/fontawesome-free/css/solid.min.css";
+
+import Animais from "./views/animais/Animais";
+import FormProntuario from "./views/form-prontuario/FormProntuario";
 import FormHistoricoClinico from "./views/form-historico-clinico/FormHistoricoClinico";
 
 export default function Certvet() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/painel/animais" element={<Animais/>}/>
                 <Route path="/painel/prontuario/cadastrar" element={<FormProntuario/>}/>
                 <Route path="/painel/prontuario/historico-clinico/cadastrar" element={<FormHistoricoClinico/>}/>
             </Routes>

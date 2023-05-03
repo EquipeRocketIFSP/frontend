86eb367ad010f98de5f2ea8a5750bbcc6d456ccd
Thunder8598 <caiquedaniel8@gmail.com>
2023-05-02 22:39:37 -0300

    ERI-545 Implementar cadastro de lotes/estoques

--- src/app/views/stock/pages/form/Create.tsx
+++ src/app/views/stock/pages/form/Create.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {Container} from "react-bootstrap";
-import {Navigate} from "react-router-dom";
+import {Navigate, useParams} from "react-router-dom";
 import axios from "axios";
 
 import Components from "../../../../components/Components";
@@ -9,25 +9,30 @@ import Layouts from "../../../../layouts/Layouts";
 import Memory from "../../../../Memory";
 import DefaultForm from "./components/DefaultForm";
 
+interface PathVariables extends Record<string, string> {
+    medicationId: string
+}
+
 export default function Create(): JSX.Element {
     const [validationErrors, setValidationErrors] = useState<Contracts.DynamicObject<string>>({});
     const [dataStatus, setDataStatus] = useState<Contracts.FormStatus>("idle");
     const [navigateToListing, setNavigateToListing] = useState<boolean>(false);
+    const {medicationId} = useParams<PathVariables>();
 
     const onSubmit = async (formData: FormData) => {
-        await axios.post(`${process.env.REACT_APP_API_URL}/medicamento`, formData, {headers: Memory.headers});
+        await axios.post(`${process.env.REACT_APP_API_URL}/medicamento/${medicationId}/estoque`, formData, {headers: Memory.headers});
 
         setDataStatus("created");
         setTimeout(() => setNavigateToListing(true), 2000);
     }
 
     if (navigateToListing)
-        return <Navigate to="/painel/medicamentos"/>;
+        return <Navigate to={`/painel/medicamentos/${medicationId}`}/>;
 
     return (
         <Layouts.RestrictedLayout>
             <main className="pt-5">
-                <h1>Validar e adicionar um novo medicamento</h1>
+                <h1>Adicionar um novo estoque ao medicamento</h1>
 
                 <Container>
                     <Components.FormSubmit
@@ -36,7 +41,6 @@ export default function Create(): JSX.Element {
                         setValidationErrors={setValidationErrors}
                         dataStatus={dataStatus}
                         validationErrors={validationErrors}
-                        successMessageOnCreation="Requisição de inclusão de medicamento recebida"
                     >
                         <DefaultForm/>
                     </Components.FormSubmit>
--- src/app/views/stock/pages/form/components/DefaultForm.tsx
+++ src/app/views/stock/pages/form/components/DefaultForm.tsx
@@ -1,20 +1,41 @@
 import {Button, Form, Row, Spinner} from "react-bootstrap";
 import React, {useContext} from "react";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import Components from "../../../../../components/Components";
-import Helpers from "../../../../../helpers/Helpers";
+
+interface PathVariables extends Record<string, string> {
+    medicationId: string
+}
 
 export default function DefaultForm(): JSX.Element {
     const {sendingForm, validationErrors} = useContext(Components.FormSubmitContext);
+    const {medicationId} = useParams<PathVariables>();
 
     return (
         <>
             <Row>
-                <Form.Group className="mb-3 col-lg-12">
-                    <Form.Label htmlFor="codigo_registro">Código de registro</Form.Label>
-                    <Form.Control name="codigo_registro" id="codigo_registro" maxLength={255}
-                                  onInput={Helpers.Masks.number} required/>
-                    <Form.Text className="text-danger">{validationErrors["codigo_registro"] ?? ""}</Form.Text>
+                <Form.Group className="mb-3 col-lg-6">
+                    <Form.Label htmlFor="lote">Lote</Form.Label>
+                    <Form.Control name="lote" id="lote" maxLength={255} required/>
+                    <Form.Text className="text-danger">{validationErrors["lote"] ?? ""}</Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3 col-lg-6">
+                    <Form.Label htmlFor="validade">Validade</Form.Label>
+                    <Form.Control type="date" name="validade" id="validade" required/>
+                    <Form.Text className="text-danger">{validationErrors["validade"] ?? ""}</Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3 col-lg-6">
+                    <Form.Label htmlFor="quantidade">Quantidade</Form.Label>
+                    <Form.Control type="number" step="0.01" name="quantidade" id="quantidade" maxLength={255} required/>
+                    <Form.Text className="text-danger">{validationErrors["quantidade"] ?? ""}</Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3 col-lg-6">
+                    <Form.Label htmlFor="medida">Medida</Form.Label>
+                    <Form.Control name="medida" id="medida" maxLength={255} required/>
+                    <Form.Text className="text-danger">{validationErrors["medida"] ?? ""}</Form.Text>
                 </Form.Group>
             </Row>
 
@@ -30,7 +51,8 @@ export default function DefaultForm(): JSX.Element {
                     ) :
                     (
                         <div className="d-flex justify-content-between">
-                            <Link to="/painel/medicamentos" className="btn btn-outline-secondary">Voltar</Link>
+                            <Link to={`/painel/medicamentos/${medicationId}`}
+                                  className="btn btn-outline-secondary">Voltar</Link>
                             <Button type="submit" variant="outline-success">Finalizar</Button>
                         </div>
                     )

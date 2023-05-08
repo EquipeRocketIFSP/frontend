3e42445cc47893ba0fa735ad072063ba28d0dd56
Thunder8598 <caiquedaniel8@gmail.com>
2023-05-07 21:59:11 -0300

    ERI-553 Adicionar exemplos de medida no placeholder do campo Medida no estoque

--- src/app/views/stock/pages/form/components/DefaultForm.tsx
+++ src/app/views/stock/pages/form/components/DefaultForm.tsx
@@ -21,26 +21,28 @@ export default function DefaultForm(props: Props): JSX.Element {
         <>
             <Row>
                 <Form.Group className="mb-3 col-lg-6">
-                    <Form.Label htmlFor="lote">Lote</Form.Label>
+                    <Form.Label htmlFor="lote">Lote*</Form.Label>
                     <Form.Control name="lote" id="lote" defaultValue={data?.lote} maxLength={255} required/>
                     <Form.Text className="text-danger">{validationErrors["lote"] ?? ""}</Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3 col-lg-6">
-                    <Form.Label htmlFor="validade">Validade</Form.Label>
+                    <Form.Label htmlFor="validade">Validade*</Form.Label>
                     <Form.Control type="date" name="validade" id="validade" defaultValue={data?.validade} required/>
                     <Form.Text className="text-danger">{validationErrors["validade"] ?? ""}</Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3 col-lg-6">
-                    <Form.Label htmlFor="quantidade">Quantidade</Form.Label>
-                    <Form.Control type="number" step="0.01" name="quantidade" id="quantidade" defaultValue={data?.quantidade} maxLength={255} required/>
+                    <Form.Label htmlFor="quantidade">Quantidade*</Form.Label>
+                    <Form.Control type="number" step="0.01" name="quantidade" id="quantidade"
+                                  defaultValue={data?.quantidade} min={0} required/>
                     <Form.Text className="text-danger">{validationErrors["quantidade"] ?? ""}</Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3 col-lg-6">
-                    <Form.Label htmlFor="medida">Medida</Form.Label>
-                    <Form.Control name="medida" id="medida" defaultValue={data?.medida} maxLength={255} required/>
+                    <Form.Label htmlFor="medida">Medida*</Form.Label>
+                    <Form.Control name="medida" id="medida" defaultValue={data?.medida} maxLength={255}
+                                  placeholder="ex: comprimido, capsula, ml" required/>
                     <Form.Text className="text-danger">{validationErrors["medida"] ?? ""}</Form.Text>
                 </Form.Group>
             </Row>

b79ce90fb0aa0ad2dd4b9db8234640e33eb4f23a
Marcos Querino <marcos.querino@aluno.ifsp.edu.br>
2023-05-24 01:47:53 -0300

    Finaliza testes do componente da página de login.

--- src/app/views/auth/pages/login/Login.test.js
+++ src/app/views/auth/pages/login/Login.test.js
@@ -1,13 +1,34 @@
-import { render, screen } from "@testing-library/react";
+import "@testing-library/react/dont-cleanup-after-each";
+import { render, screen, cleanup } from "@testing-library/react";
 import { MemoryRouter } from 'react-router-dom';
 import Login from "./Login";
 
-describe("Formulário de login de acordo com DTO", () => {
+describe("Login", () => {
 
+    afterAll(() => {
+        cleanup();
+    });
     render(<Login />, { wrapper: MemoryRouter });
 
-    test("Campo do código da clínica", () => {
+    test("Deve conter o campo do código da clínica", () => {
         const clinica = screen.getByPlaceholderText('Código da Clínica');
         expect(clinica).toHaveAttribute("name", "clinica");
     });
+
+    test("Deve conter o campo E-mail", () => {
+        const clinica = screen.getByPlaceholderText('E-mail');
+        expect(clinica).toHaveAttribute("name", "email");
+    });
+
+    test("Deve conter o campo Senha", () => {
+        const clinica = screen.getByPlaceholderText('Senha');
+        expect(clinica).toHaveAttribute("name", "senha");
+    });
+
+    test("Deve conter botão de enviar", () => {
+        const btn = screen.getByRole('button', { type: /submit/i });
+        expect(btn).toBeInTheDocument();
+
+    });
+
 });
